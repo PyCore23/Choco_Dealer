9036b3f203e2b2cb75d5ca167260919253e5e2e6
yes <yes@yess-MacBook-Pro.local>
2024-02-22 06:49:15 +0200

    alpha PVP ver 0.01

---
+++ run.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    return render_template("main_page.html")
+
+
+if __name__ == "__main__":
+    app.run()
